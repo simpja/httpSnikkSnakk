3a6db9cffa495079739c78538bf667dd691cce43
simpja <simenpjaat@gmail.com>
2019-08-16 23:07:20 +0200

    server: add API endpoint

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-var http = require('http');
+var http = require("http");
 var PORT = 8080;
-var fs = require('fs');
+var fs = require("fs");
+var path = require("path");
 
 function getHTMLAsString(fileName, callBack) {
-  fs.readFile(fileName, 'utf8', function(err, data) {
+  fs.readFile(fileName, "utf8", function(err, data) {
     if (err) {
-      callBack('Could not retrieve file');
+      callBack("Could not retrieve file");
     }
     callBack(data);
   });
@@ -14,12 +15,25 @@ function getHTMLAsString(fileName, callBack) {
 //create a server object:
 http
   .createServer(function(req, res) {
-    console.log('hey! someone is contacting me!', new Date().getSeconds());
-    getHTMLAsString('./index.html', HTMLString => {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(HTMLString); //write a response to the client
-      res.end(); //end the response
-    });
+    console.log(
+      "hey! someone is contacting me!",
+      new Date().getSeconds(),
+      req.url
+    );
+    if (req.url === "/") {
+      getHTMLAsString("./index.html", HTMLString => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(HTMLString); //write a response to the client
+        res.end(); //end the response
+      });
+    } else if (req.url === "/api") {
+      res.write("WTF bro, this endpoint is on the toilet");
+      res.end();
+    } else {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.write("404: Page not existus gigantus dickus");
+      res.end();
+    }
   })
   .listen(PORT); //the server object listens on port 8080
 console.log(`Server is running on port: ${PORT}`);
