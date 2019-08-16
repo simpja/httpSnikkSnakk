244faac0fa5f188f604ab5de1e77ee779eb19d7c
simpja <simenpjaat@gmail.com>
2019-08-16 22:28:48 +0200

    server gives client html and its amazing

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 var http = require("http");
 var PORT = 8080;
+var fs = require("fs");
+
+function getHTMLAsString(fileName, callBack) {
+  fs.readFile(fileName, "utf8", function(err, data) {
+    if (err) {
+      callBack("Could not retrieve file");
+    }
+    callBack(data);
+  });
+}
 
 //create a server object:
 http
   .createServer(function(req, res) {
-    console.log(req.body);
-    res.write("Hello, your IP is: " + req.connection.remoteAddress); //write a response to the client
-    res.end(); //end the response
+    getHTMLAsString("./index.html", HTMLString => {
+      console.log(HTMLString);
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(HTMLString); //write a response to the client
+      res.end(); //end the response
+    });
   })
   .listen(PORT); //the server object listens on port 8080
 console.log(`Server is running on port: ${PORT}`);
